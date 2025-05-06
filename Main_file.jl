e7a66c0111900f273de19476bb803bac4ecd491e
Jonatan Eklöv <jonatan.eklov@gmail.com>
2025-05-06 11:20:33 +0200

    Fixat phase-diagram för de 4 fallen

--- Main_file.jl
+++ Main_file.jl
@@ -4,17 +4,19 @@ using Plots
 using LinearAlgebra
 print("Hello")
  
-r_1 = 1
-r_2 = 2
-K_1 = 4
-K_2 = 5
-a_1 = 1
-a_2 = 1
+r_1 = 2
+r_2 = 2  
+K_1 = 1
+K_2 = 1
+
+a_1 = 1/2
+a_2 = 1/2
 
 
 # Solution over time
-f(x) = [r_1*x[1]* (1-x[1]/K_1) - a_1*x[1]*x[2],
+f(dx,x,p,t) = [r_1*x[1]* (1-x[1]/K_1) - a_1*x[1]*x[2],
         r_2*x[2]* (1-x[2]/K_2) - a_2*x[2]*x[1] ]
+
 tspan = 0,10
 x0 = [2, 1]
 prob = ODEProblem(f,x0,tspan)
@@ -25,7 +27,7 @@ plot(sol)
 
 # Creating vector field
 min_x = 0
-max_x = 3
+max_x = 2
 step_s = (max_x-min_x)/20
 
 meshgrid(x, y) = (repeat(x, outer=length(y)), repeat(y, inner=length(x)))
@@ -35,14 +37,14 @@ x1, x2 = meshgrid(min_x:step_s:max_x, min_x:step_s:max_x)
 u1 = similar(x1)
 u2 = similar(x2)
 for i in eachindex(x1)
-    dx = f([x1[i], x2[i]])
+    dx = f(0, [x1[i], x2[i]], 0, 0)
     norm_dx = norm(dx) + 1e-8
     norm_dx = 20
     u1[i] = dx[1] / norm_dx
     u2[i] = dx[2] / norm_dx
 end
 
-# Ploting vector field
+# Plotting vector field
 quiver(x1, x2, quiver=(u1, u2), aspect_ratio=1)
 
 # CREATING NULLCLINES
